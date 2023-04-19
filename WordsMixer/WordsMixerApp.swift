7b72897a607b4dada1407844ca5ec12c36dd10f5
António Almeida <antonio95@icloud.com>
2023-04-19 22:34:10 +0100

    Initial Commit

---
+++ WordsMixer/WordsMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsMixerApp.swift
+//  WordsMixer
+//
+//  Created by António Almeida on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordsMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
